8a9c7e16075604042d02ba8ed215150db332c4d0
Daddy2054 <stivali-tinte-0w@icloud.com>
2025-10-21 15:57:57 +0200

    show swagger in production

--- todoapi2/Program.cs
+++ todoapi2/Program.cs
@@ -14,14 +14,14 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.MapOpenApi();
     app.UseSwaggerUi(options =>
 {
     options.DocumentPath = "/openapi/v1.json";
 });
-}
+// }
 
 app.UseHttpsRedirection();
 
